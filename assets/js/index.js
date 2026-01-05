e6e48b97c6c2121904a1c38108634cd18aec2d3b
Arthur Piloto <arthurpiloto.github@gmail.com>
2026-01-05 15:37:51 -0300

    editais page structure done, missing true content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,4 @@
 // JS geral, todos os scripts se encontram aqui
-
-/* --- INDEX --- */
-// SLIDER
-let slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
-setInterval(() => {
-  plusSlides(1);
-}, 5000);
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-if (prevBtn && nextBtn) {
-  prevBtn.addEventListener("click", () => {
-    plusSlides(-1);
-  });
-
-  nextBtn.addEventListener("click", () => {
-    plusSlides(1);
-  });
-}
-
 /* --- UFSJ --- */
 // Obrigatório usar DOMContentLoaded pois, senão, os elementos não são lidos corretamente
 document.addEventListener("DOMContentLoaded", function () {
@@ -123,3 +83,42 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+/* --- INDEX --- */
+// SLIDER
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+setInterval(() => {
+  plusSlides(1);
+}, 5000);
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+if (prevBtn && nextBtn) {
+  prevBtn.addEventListener("click", () => {
+    plusSlides(-1);
+  });
+
+  nextBtn.addEventListener("click", () => {
+    plusSlides(1);
+  });
+}
